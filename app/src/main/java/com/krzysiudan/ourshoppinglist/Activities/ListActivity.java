4ca1801c38b0ba22eb2bf47481b64b21269670bb
Krzysiudan <krzysiudan@gmail.com>
2020-01-05 17:46:34 +0100

    Warnings removing in ListActivity

--- app/src/main/java/com/krzysiudan/ourshoppinglist/Activities/ListActivity.java
+++ app/src/main/java/com/krzysiudan/ourshoppinglist/Activities/ListActivity.java
@@ -2,15 +2,12 @@ package com.krzysiudan.ourshoppinglist.Activities;
 
 
 import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
+
 import androidx.annotation.Nullable;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
@@ -20,17 +17,14 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.Toolbar;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.UserProfileChangeRequest;
 import com.krzysiudan.ourshoppinglist.Adapters.RecyclerAdapterShoppingList;
 import com.krzysiudan.ourshoppinglist.Fragments.DialogAddList;
 import com.krzysiudan.ourshoppinglist.R;
