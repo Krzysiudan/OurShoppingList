a38789bd2a644cd1580cfc2f7d321f277036c7cf
Krzysiudan <krzysiudan@gmail.com>
2020-01-16 01:25:37 +0100

    DialogShareList now won't close after one invitation

--- app/src/main/java/com/krzysiudan/ourshoppinglist/Fragments/DialogShareList.java
+++ app/src/main/java/com/krzysiudan/ourshoppinglist/Fragments/DialogShareList.java
@@ -7,15 +7,20 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.DialogFragment;
 
 
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.WriteBatch;
+
 import com.krzysiudan.ourshoppinglist.DatabaseItems.ShoppingList;
 import com.krzysiudan.ourshoppinglist.R;
 
@@ -49,24 +54,45 @@ public class DialogShareList extends DialogFragment {
         final View mView = mLayoutInflater.inflate(R.layout.dialog_custom_share_list,null);
         EditText alertEditText = mView.findViewById(R.id.fragment_dialog_share_editText);
 
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
         AlertDialog.Builder mBuilder = new AlertDialog.Builder(getActivity());
         mBuilder.setView(mView);
         mBuilder.setPositiveButton(R.string.add, (dialogInterface, i) -> {
-            final String friendEmail = alertEditText.getText().toString();
-            if(!friendEmail.equals("")){
-                final DocumentReference mDocumentReference = FirebaseFirestore.getInstance().collection("users").document(friendEmail).collection("usedLists").document(key);
-                Map<String, Object> data = new HashMap<>();
-                WriteBatch mWriteBatch = FirebaseFirestore.getInstance().batch();
-                mWriteBatch.set(mDocumentReference,mShoppingList);
-                mWriteBatch.commit()
-                        .addOnCompleteListener(task ->
-                                Log.d("OurShoppingList","Batch in DialogShareList completed"))
-                        .addOnFailureListener(e ->
-                                Log.d("OurShoppingList","Batch in DialogShareList failure"));
-            }
-        });
+
+                });
         mBuilder.setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
         });
-        return mBuilder.create();
+        final AlertDialog dialog = mBuilder.create();
+        dialog.show();
+        View parentView = getActivity().findViewById(R.id.activity_list_coordinatorLayout);
+        dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(v -> {
+            final String friendEmail = alertEditText.getText().toString();
+            if (!friendEmail.equals("") && user.getEmail() != friendEmail) {
+                final DocumentReference mDocumentReference = FirebaseFirestore.getInstance().collection("users").document(friendEmail).collection("usedLists").document(key);
+                DocumentReference friendDocument = FirebaseFirestore.getInstance().collection("users").document(friendEmail);
+                friendDocument.get().addOnCompleteListener(task -> {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot doc = task.getResult();
+                        if (doc.exists()) {
+                            mDocumentReference.set(mShoppingList)
+                                    .addOnSuccessListener(aVoid -> {
+                                        Log.i("OurShoppingList", "List Shared with friend");
+                                        Toast.makeText(getActivity().getApplicationContext(),R.string.toast_message_dialog_share_list_invitation_send,Toast.LENGTH_SHORT).show();
+                                    })
+                                    .addOnFailureListener(e -> Log.i("OurShoppingList", "Failed sharing list with friend"));
+                        } else {
+                            Log.e("OurShoppingList", "No such document");
+                        }
+                    } else {
+                        Log.e("OurShoppingList", "Get failed with: " + task.getException());
+                    }
+                });
+            }
+
+        });
+
+        return dialog;
     }
 }
